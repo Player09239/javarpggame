fbbc61c46c08f5b3bd1de35eb1f6325e0ef18128
Chaos <destroyer09239@gmail.com>
2026-04-05 09:37:15 -0400

    Update Spirit.java

--- javastuff/javastuff/src/main/java/stuff/Entities/Mobs/Spirit.java
+++ javastuff/javastuff/src/main/java/stuff/Entities/Mobs/Spirit.java
@@ -1,1 +1,9 @@
-a
+package stuff.Entities.Mobs;
+
+import stuff.Entities.Enemy;
+
+public class Spirit extends Enemy {
+    public Spirit(int maxhp, int atk, int mincash, int maxcash) {
+        super(maxhp, atk, "Spider", mincash, maxcash);
+    }
+}
